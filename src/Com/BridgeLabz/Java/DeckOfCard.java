195b3372b7dbea02f180b2a68537b68fbb6f8fcb
Avinash <aj912873@gmail.com>
2022-03-07 08:54:49 +0530

    Added: Deck Of Cards Created

--- src/Com/BridgeLabz/Java/DeckOfCard.java
+++ src/Com/BridgeLabz/Java/DeckOfCard.java
@@ -1,8 +1,22 @@
 package Com.BridgeLabz.Java;
 
 public class DeckOfCard {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Deck Of Card Java Project");
-	}
-
+	String[] deck = new String[52];
+    String[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
+    String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+    
+    public void alignCards() {
+    	int deckIndex = 0;
+    	for (int i = 0; i < suit.length; i++) {
+    	    for (int j = 0; j < rank.length; j++) {
+    		deck[deckIndex] = rank[j] + " of " + suit[i];
+    		deckIndex++;
+    	    }
+    	}
+    }	
+    	    
+    
+    	    public static void main(String[] args) {
+    	System.out.println("Welcome to Deck Of Card Java Project");
+    }
 }
